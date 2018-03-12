baab06eabea43f7fc023bff18f59860049b7c0e8
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-03-12 15:47:40 -0300

    Modularização do projeto. Feito tudo até a seção 8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import {Routes} from '@angular/router'
 
 import {HomeComponent} from './home/home.component'
-import {AboutComponent} from './about/about.component'
 import { RestaurantsComponent } from 'app/restaurants/restaurants.component';
 import { RestaurantDetailComponent } from 'app/restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 
 export const ROUTES: Routes = [
@@ -18,7 +16,7 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
         ]},
-    {path: 'order', component: OrderComponent},
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
     {path: 'order-summary', component: OrderSumaryComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { OrderService } from "../order/order.service";
+
+@NgModule({
+  declarations:[
+    InputComponent,
+    RadioComponent,
+    RatingComponent
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  exports: [
+    InputComponent,
+    RadioComponent,
+    RatingComponent,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers:[
+        ShoppingCartService,
+        RestaurantsService,
+        OrderService
+      ]
+    }
+  }
+}
